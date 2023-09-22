80bddbd8df2fab29efefedec47ad7c422ad76474
greeenOrange <gm1sadiq@gmail.com>
2023-09-23 02:23:15 +0600

    json

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080;
+const uuid = require("uuid"); // Import the 'uuid' library
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser); // Enable JSON body parsing
+
+// Custom middleware to add a UUID to POST requests
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.id = uuid.v4(); // Add a UUID to the new resource
+  }
+  // Continue processing the request
+  next();
+});
+
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
