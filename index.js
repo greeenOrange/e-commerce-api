ddc9aff53452632ce32b2b78b330674419f8b7a5
greeenOrange <gm1sadiq@gmail.com>
2023-09-23 00:35:38 +0600

    index

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+
+async function run() {
+    try {
+      await client.connect();
+} 
+  finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+  
+  app.get('/', (req,res) =>{
+    res.send('daily-e-commerce')
+});
+app.listen(port, () =>{
+    console.log('Running the server on Port', port);
+})
